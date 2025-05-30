8f22ede57c7e1c413d02109d2715f00fa9677275
MTKachenko <11Draghost11@gmail.com>
2025-05-30 12:54:58 +0300

    input add

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const NUMBERS_CONTAINER = document.querySelector('.numbers');
 const INPUT_CONTAINER = document.querySelector('.input-container');
+const input_array = [];
 
 const onNumberClick = (e) => {
     const { target } = e;
+    if(target.classList.contains('number-el') && target.innerHTML !== `&lt;`) {
+        console.log()
+     input_array.push(target.innerHTML)
+     INPUT_CONTAINER.innerHTML = input_array.join(' ')
+    }
+    
     
 }   
 

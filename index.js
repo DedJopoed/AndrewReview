851371934886e19c657e508ad11f61febdc9c7ea
MTKachenko <11Draghost11@gmail.com>
2025-05-30 13:23:52 +0300

    add delete func in input

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const input_array = [];
 
 const onNumberClick = (e) => {
     const { target } = e;
-    if(target.classList.contains('number-el') && target.innerHTML !== `&lt;`) {
-        console.log()
-     input_array.push(target.innerHTML)
-     INPUT_CONTAINER.innerHTML = input_array.join(' ')
+    if (target.classList.contains('number-el')) {
+        if(target.innerHTML !== `&lt;`) {
+            input_array.push(target.innerHTML)
+        }
+        else if(target.innerHTML == `&lt;`) {
+            input_array.pop()
+        }
+        
+        INPUT_CONTAINER.innerHTML = input_array.join(' ')
     }
-    
     
 }   
 
